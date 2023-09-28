2414389c39b0f1bc7ecb46010a42286879deb419
TomasMarina <tomasimarina@gmail.com>
2023-09-28 17:49:57 -0300

    Save data

--- R/1_Data_Load&Tidy.R
+++ R/1_Data_Load&Tidy.R
@@ -1,7 +1,12 @@
+# Load & tidy data
+# Data: September 2023
+# Author: Tomas I. Marina
+
 
 # Load packages -----------------------------------------------------------
 require(tidyverse)
 require(janitor)
+
 
 # Load data ---------------------------------------------------------------
 
@@ -15,6 +20,7 @@ magellan_dup <- read.csv(file = "Data/MagellanStrait_links.csv", header = TRUE) 
 magellan_ok <- magellan_df %>%
   distinct()
 
+
 # Sim & Diff --------------------------------------------------------------
 
 ## Interactions
@@ -23,11 +29,13 @@ beagle_ulinks <- dplyr::anti_join(beagle_df, magellan_ok)
 magellan_ulinks <- dplyr::anti_join(magellan_ok, beagle_df)
 
 ## Species
+### Beagle
 b_res <- unique(beagle_df$resource)
 b_con <- unique(beagle_df$consumer)
 b_sp <- as.data.frame(c(b_res, b_con)) %>% 
   distinct() %>% 
   rename(., Species = "c(b_res, b_con)")
+### Magellan
 m_res <- unique(magellan_ok$resource)
 m_con <- unique(magellan_ok$consumer)
 m_sp <- as.data.frame(c(m_res, m_con)) %>% 
@@ -39,4 +47,7 @@ beagle_usp <- dplyr::anti_join(b_sp, m_sp)
 magellan_usp <- dplyr::anti_join(m_sp, b_sp)
 
 
+# Save data ---------------------------------------------------------------
+save(beagle_df, magellan_ok, b_sp, m_sp,
+     file = "Results/Data_tidy.rda")
 
